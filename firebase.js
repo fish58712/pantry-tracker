8317da4e98420727d6610a6a05c3f7705ff243e0
fish58712 <fish58712@gmail.com>
2024-08-04 17:24:22 -0500

    Update Firebase configuration to use environment variables

--- firebase.js
+++ firebase.js
@@ -12,15 +12,7 @@ import {getFirestore} from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-const firebaseConfig = {
-    apiKey: "AIzaSyD7fOdSvFYp5JCaPi9Pz_hDmWYfxfJd05c",
-    authDomain: "pantry-c2772.firebaseapp.com",
-    projectId: "pantry-c2772",
-    storageBucket: "pantry-c2772.appspot.com",
-    messagingSenderId: "811623403533",
-    appId: "1:811623403533:web:4bff5eb3c75266c3324544",
-    measurementId: "G-5RX7MH893D"
-  };
+
 
 // Initialize Firebase
 
