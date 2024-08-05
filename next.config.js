c0c962100aeb44a2ec73617ada6ef3de5d3e3e97
Nimo <136946359+fish58712@users.noreply.github.com>
2024-08-05 02:49:16 +0000

    Update next.config.js with correct settings

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// next.config.js
+
+const nextConfig = {
+    experimental: {
+      appDir: true, // Enables the experimental app directory
+    },
+    // Add any other configurations here
+  }
+  
+  module.exports = nextConfig
+  
